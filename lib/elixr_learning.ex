e103b485e659dc0870496e6d54edcce09c19f67b
john nicholas <themrtortoise@gmail.com>
2017-01-18 11:33:37 -0800

    turned into a project

---
+++ lib/elixr_learning.ex
@@ -0,0 +1,18 @@
+defmodule ElixrLearning do
+  @moduledoc """
+  Documentation for ElixrLearning.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixrLearning.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixr_learning_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixrLearningTest do
+  use ExUnit.Case
+  doctest ElixrLearning
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
